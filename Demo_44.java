f8d63702269650153cdfd1bdb591f3919278c6b0
Atif Khan <khanatif271@gmail.com>
2021-02-13 20:03:07 +0530

    initial comit

---
+++ Demo_44.java
@@ -0,0 +1,8 @@
+// A program to take command line input and convert it int integer type and then print it
+class Demo_44{
+	public static void main(String args[]){
+		int i;
+		i=Integer.parseInt(args[0]);
+		System.out.println("The value of integer is "+i);
+	}
+}
---
+++ RecursiveFibonacci.java
@@ -0,0 +1,16 @@
+class RecursiveFibonacci{
+	int x;
+	int fibo(int a){
+		if(a==0) return 0;
+		else if(a==1) return 1;
+		else return(fibo(a-1)+fibo(a-2));
+	}
+	public static void main(String []args){
+		RecursiveFibonacci fibo1= new RecursiveFibonacci();
+		fibo1.x=Integer.parseInt(args[0]);
+		for(int i=0;i<fibo1.x;i++)
+		{
+			System.out.println(fibo1.fibo(i));
+		}
+	}
+}
